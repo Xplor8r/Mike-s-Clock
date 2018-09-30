bbcd86e9c8ad582ce7175d4679493518943f68bc
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-30 16:25:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
+
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-    var date = new Date();
-    console.log(date);
-    let hr = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
-
-    let hrPosition = (hr*360/12)+(min*(360/60)/12);
-    let minPosition = (min*360/60)+(sec*(360/60)/60);
-    let secPosition = sec*360/60;
-
 function runTheClock(){
-   
-    hrPosition = hrPosition(3/360);
-    minPosition = minPosition(6/60);
-    secPosition = secPosition+6;
-    
-    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 
+
+  var date = new Date(); //date object
+
+  //get hour, minutes and seconds from date object
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+
+ 
+  let hrPosition = (hr*30)+(min*30/60); // divide 1 hour segment by 60 minutes
+  let minPosition = (min*6)+(sec*6/60); // divide 1 min section by 60 seconds
+  let secPosition = sec*6;
+
+
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
-var interval = setInterval(runTheClock, 1000);
 
+var interval = setInterval(runTheClock, 1000); //1000 milliseconds is 1 second.
